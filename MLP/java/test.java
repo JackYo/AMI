af6713dc386c270641b6a4fb1db9203af19982e0
Jack <frpsjack@gmail.com>
2017-03-23 05:08:14 +0800

    newest

--- MLP/java/test.java
+++ MLP/java/test.java
@@ -4,40 +4,25 @@ class test{
 	public static void main(String a[]){
 		try{
 		 
-			String prg = "import sys\nprint(int(sys.argv[1])+int(sys.argv[2]))\n";
+			/*String prg = "import sys\nprint(int(sys.argv[1])+int(sys.argv[2]))\n";
 			BufferedWriter out = new BufferedWriter(new FileWriter("test2.py"));
 			out.write(prg);
-			out.close();
+			out.close();*/
 			int number1 = 10;
 			int number2 = 32;
 			 
-			ProcessBuilder pb = new ProcessBuilder("python","test2.py",""+number1,""+number2);
+			//ProcessBuilder pb = new ProcessBuilder("python","test.py",""+number1,""+number2);
+			//ProcessBuilder pb = new ProcessBuilder("python","mlp_test.py");
+			ProcessBuilder pb = new ProcessBuilder("python","../python/mlp_test.py");
 			Process p = pb.start();
 			 
+			String line;
 			BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			int ret = new Integer(in.readLine()).intValue();
-			System.out.println("value is : "+ret);
+			while ((line = in.readLine()) != null) {
+				
+				System.out.println(line);
+			}
+			System.out.println("\npython finished");
 		}catch(Exception e){System.out.println(e);}
 	}
 }
-/*
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-publicclass Test5 {
-publicstaticvoid main(String args){
-	try{
-		System.out.println("start");
-		Process pr = Runtime.getRuntime.exec("python test.py");
-		BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream));
-		String line;
-		while ((line = in.readLine) != null) {
-			System.out.println(line);
-		}
-		in.close;
-		pr.waitFor;
-		"end");
-	} catch (Exception e){
-		e.printStackTrace;
-	}
-}
-*/
