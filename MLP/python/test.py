5043e3ea699260952659d945b212f90f35282cf6
Jack <frpsjack@gmail.com>
2017-03-28 07:24:58 +0800

    recurrent network

--- MLP/python/test.py
+++ MLP/python/test.py
@@ -1,4 +1,9 @@
+import sys
+sys.path.insert(0, "./pyrenn")
 from datetime import datetime, timedelta
+from pyrenn.python import pyrenn
+
+
 
 time_object = datetime.strptime("2015/1/1  00:00:00", '%Y/%m/%d  %H:%M:%S')
 for i in range(0,96):
